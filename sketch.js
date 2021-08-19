2017a139e690392b69820e6be8b531eb2cf50615
kikugaw <k1kugawa07@gmail.com>
2021-08-19 14:55:41 +0900

    add button

--- sketch.js
+++ sketch.js
@@ -1,109 +1,223 @@
+let score = 0;
+let ex = 0;
+let ey = 200;
+let e2x = 700;
+let e2Size = 35;
+let eSize;
+let eSpeed;
+let px = 400;
+let py = 200;
+let pSize = 50;
+let jValue = 0;
+let hValue = 0;
+let sValue = 1;
+let pPosition = 0;
+let red = 0;
+let green = 0;
+let blue = 0;
+let bvalue = 0;
+let img;
 let song;
-let eSize=50;//敵の大きさ
-let eSpeed=2;//敵のスピード
-let ex=0;//敵のx座標
-let ey=200;//敵のy座標
-let px=500;//自機のx座標
-let py=200;//自機のy座標
-let pSize=50;//自機の大きさ
-let jumpFlag=0;//ジャンプのフラグ。oは地上1が上昇2が下降。
-let endFlag = 0;
 
 function preload(){
-    song = loadSound('./assets/music/bgm.mp3');
+  song = loadSound('./assets/music/bgm.mp3');
 }
 
 function setup() {
-    let distance = dist(px,py,ex,ey);//distはpx,pyからex,eyの距離
-    createCanvas(600, 400);
-    // background(255, 0, 0);
-    song.loop();
+  // put setup code here
+  createCanvas(600, 400);
+  song.loop();
+  eCreate();
 }
 
-function draw(){
-    background(255);
-    gameDisplay();
-    if(endFlag==0){
-        enemyDisplay();//敵の描写
-        playerDisplay();//自分の描写
-        hit();//当たり判定
-      }else{
-        gameOver();//ゲームオーバー
-      }
-    if(jumpFlag>0){
-        jump();
-      }
+function draw() {
+  // put drawing code here
+  background(255);
+  ///背景画像挿入
+  // image(img, 0, -50, 450, 200);
+  gameDisplay();
+  if (hValue == 0) {
+    if (bvalue == 0) {
+      eBall();
+    } else {
+      eball2();
+    }
+    pBall();
+    if (jValue > 0) {
+      jump();
+    }
+    hit();
+    hit2();
+  } else {
+    gameOver();
+  }
 }
 
-function gameDisplay(){
+function gameDisplay() {
+  fill(0);
+  fill(red, 255 - green, 255 - blue);
+  rect(0, 200, 600, 200);
+  fill(0);
+  textSize(32);
+  text("Score" + score, 425, 25);
+}
+
+function jump() {
+  if (jValue == 1 && py > 125) {
+    py = py - 1;
+  } else {
+    jValue = 2;
+  }
+  if (jValue == 2 && py < 200) {
+    py = py + 1;
+  } else if (jValue == 2) {
+    jValue = 0;
+  }
+}
+
+function eBall() {
+  if (score < 4) {
+    ex = ex + eSpeed;
+    fill(225, green, blue);
+    ellipse(ex, ey - eSize / 2, eSize, eSize);
+    fill(0, 0, 0);
+    // fill(225 - red, 225 - green, 225 - blue);
+  } else if(score <8){
+    
+    ex = ex + eSpeed * 1.5;
+    fill(225, green, blue);
+    ellipse(ex, ey - eSize / 2, eSize, eSize);
     fill(0);
-    rect(0, 200, 600, 200);
-    textSize(25);
-    text("score",500,25);
-      }
-
-function enemyDisplay(){
-    ex=ex+eSpeed;//敵を移動させる
+  }else{
+    ex = ex + eSpeed + eSpeed;
+    fill(225, green, blue);
+    ellipse(ex, ey - eSize / 2, eSize, eSize);
     fill(0);
-    ellipse(ex,ey-eSize/2,eSize,eSize);//敵を表示
-    if(ex>width){//円が右端より大きかったら
-        ex=0;//x軸を0にする
+  }
+  if (ex > width) {
+    ex = random(-200, -100);
+    eCreate();
+    back();
+    px = 400;
+    pCreate();
+    score = score + 1;
+  
+    bvalue = 0;
+    for (let i = 1; i < 100; i++) {
+      if (score == 3 * i) {
+        bvalue = 1;
       }
-      }
+    }
+  }
+}
 
-      function playerDisplay(){
-        ellipse(px,py-pSize/2,pSize,pSize);
-      }
+function eball2() {
+  eSpeed = 4;
+  e2x = e2x - eSpeed;
+  ellipse(e2x, ey - e2Size / 2, e2Size, e2Size);
+  if (e2x < 0) {
+    e2x = random(700, 750);
+    eCreate();
+    px = 400;
+    pCreate();
+    back();
+    score = score + 1;
+    bvalue = 0;
+  }
+}
 
-      function hit(){
-        let distance = dist(px,py,ex,ey);
-        if(distance<pSize/2+eSize/2){//敵と自機が当たった
-          endFlag=1;
-        }
-      }
-      
-      function gameOver(){
-        textSize(30);
-        text("GAME OVER",200,100);
-        song.stop();
-      }
+function pBall() {
+  if (score > 3) {
+    if (ex > 0) {
+      px = px - pPosition;
+    }
+  }
+  ellipse(px, py - pSize / 2, pSize, pSize);
+}
 
+function hit() {
+  let distance = dist(px, py, ex, ey); //距離を測る
+  if (distance < 25 + eSize / 2) {
+    hValue = 1;
+  }
+}
 
-      function jump(){
-        if((jumpFlag==1)&&(py>125)){//ジャンプ上昇でy軸が125より大きかったら
-          py=py-1;
-        }else{
-          jumpFlag=2;//円を下降させる
-        }
-        if((jumpFlag==2)&&(py<200)){//円が下降でy軸が200より小さかったら
-          py=py+1;
-        }else if(jumpFlag==2){
-          jumpFlag=0;//ジャンプフラグをリセット
-        }
-      }
+function hit2() {
+  let distance2 = dist(px, py, e2x, ey); //距離を測る
+  if (distance2 < 25 + e2Size / 2) {
+    hValue = 1;
+  }
+}
 
-      function keyPressed() {
-        if (keyCode == 32) {
-          if (jumpFlag == 0) {
-            jumpFlag = 1;
-          }
-        }
-      }
+// function gameStart() {
+//   textSize(30);
+//   fill(128 + sin(frameCount * 0.1) * 128);
+//   text("GAME Start", 200, 120);
+// }
 
-// function eCreate() {
-//     eSize = random(40, 70);
-//     eSpeed = random(1, 3);
-//       }
+function gameOver() {
+  textSize(30);
+  fill(128 + sin(frameCount * 0.1) * 128);
+  text("GAME OVER", 200, 120);
+  song.stop();
+}
 
+function eCreate() {
+  eSize = random(40, 70);
+  eSpeed = random(1, 3);
+  red = random(0, 255);
+  green = random(0, 255);
+  blue = random(0, 255);
+}
+
+function pCreate() {
+  pPosition = random(0, 1);
+}
+
+function reset() {
+  document.location.reload();
+}
+
+function jumping() {
+  if (jValue == 0) {
+    jValue = 1;
+  }
+}
+
+function keyPressed() {
+  if (keyCode == 32) {
+    if (jValue == 0) {
+      jValue = 1;
+    }
+  }
+}
 
 // function mousePressed() {
-//     if (song.isPlaying()) {
-//         // .isPlaying()はブール値を返す
-//         song.stop();
-//         background(255, 0, 0);
-//     }
-//     else {
-//         song.play();
-//         background(0, 255, 0);
-//     }
+// //   noLoop();
 // }
+// function mouseReleased() {
+//   loop();
+// }
+
+
+function back() {
+  bvalue = random(0, 1);
+}
+
+function togglePlay(){
+  if (song.isPlaying()) {
+    // .isPlaying()はブール値を返す
+    song.stop();
+}
+else {
+    song.play();
+}
+}
+function touchStarted() {
+  if (!isUserStarted) {
+      // touchStarted()を1回だけ呼び出されるようにする
+      print('touch');
+      userStartAudio();
+      isUserStarted = true;
+  }
+}
